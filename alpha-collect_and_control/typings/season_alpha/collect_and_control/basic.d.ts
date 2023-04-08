f9657dcc70a3ee27cebc47b2e70200484a17b854
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-04-08 13:09:50 +0200

    arenas

---
+++ alpha-collect_and_control/typings/season_alpha/collect_and_control/basic.d.ts
@@ -0,0 +1,4 @@
+declare module "arena/season_alpha/collect_and_control/basic" {
+    export * from "arena/season_alpha/collect_and_control/basic/prototypes";
+    export * from "arena/season_alpha/collect_and_control/basic/constants";
+}
